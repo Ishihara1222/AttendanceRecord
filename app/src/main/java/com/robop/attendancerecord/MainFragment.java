74fa51793e60625f282e5b1a4525580f4a084230
Dai <dai7774@gmail.com>
2018-02-08 02:15:53 +0900

    fragment file check

---
+++ app/src/main/java/com/robop/attendancerecord/MainFragment.java
@@ -0,0 +1,15 @@
+package com.robop.attendancerecord;
+
+import android.app.Fragment;
+
+/**
+ * Created by daipc on 18/02/08.
+ */
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance(){
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+}
