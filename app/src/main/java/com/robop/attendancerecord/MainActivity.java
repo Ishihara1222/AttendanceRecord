daca8c94deaa4b3df707b7fadc7ad5e956855297
Dai <dai7774@gmail.com>
2018-02-08 01:34:36 +0900

    ただの通知表示実装

--- app/src/main/java/com/robop/attendancerecord/MainActivity.java
+++ app/src/main/java/com/robop/attendancerecord/MainActivity.java
@@ -1,7 +1,12 @@
 package com.robop.attendancerecord;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        attendanceNotification();
+    }
+
+    private void attendanceNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("出席しましたか？");
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+
+        NotificationManager manager = (NotificationManager)getSystemService(Service.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
     }
 }
