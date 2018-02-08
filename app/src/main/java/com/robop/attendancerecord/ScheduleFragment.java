300fcc1b3a01b03eee15db7ec459f3ccc8752b66
Dai <dai7774@gmail.com>
2018-02-08 15:57:13 +0900

    ListView Layout created

--- app/src/main/java/com/robop/attendancerecord/ScheduleFragment.java
+++ app/src/main/java/com/robop/attendancerecord/ScheduleFragment.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ScheduleFragment extends Fragment {
+
+    ArrayList<CustomScheduleInfoListItem> listItems;
 
     public static ScheduleFragment newInstance(){
         ScheduleFragment fragment = new ScheduleFragment();
@@ -15,6 +20,22 @@ public class ScheduleFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        listItems = new ArrayList<>();
         return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView scheduleListView = (ListView) view.findViewById(R.id.scheduleList);
+
+        for (int i=0; i<5; i++){
+            CustomScheduleInfoListItem item = new CustomScheduleInfoListItem("教科名", 3, 4);
+            listItems.add(item);
+        }
+
+        CustomScheduleListAdapter adapter = new CustomScheduleListAdapter(this.getActivity(), R.layout.schedule_list_item, listItems);
+        scheduleListView.setAdapter(adapter);
     }
 }
