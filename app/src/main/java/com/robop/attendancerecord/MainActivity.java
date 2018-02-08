0f036aca60e2cf61ed69a947c7b99b91c60863b3
Dai <dai7774@gmail.com>
2018-02-08 11:00:45 +0900

    TabLayout+ViewPager実装

--- app/src/main/java/com/robop/attendancerecord/MainActivity.java
+++ app/src/main/java/com/robop/attendancerecord/MainActivity.java
@@ -1,16 +1,55 @@
 package com.robop.attendancerecord;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    CustomFragmentAdapter customFragmentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] tabNames = getResources().getStringArray(R.array.tabNames);
 
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        viewPager = (ViewPager)findViewById(R.id.pager);
+
+        toolbar.setTitle("曜日");
+        setSupportActionBar(toolbar);
+
+        customFragmentAdapter = new CustomFragmentAdapter(getSupportFragmentManager(), tabNames);
+        for(int i=0; i<6; i++){
+            customFragmentAdapter.addFragment(MainFragment.newInstance());
+        }
+
+        viewPager.setAdapter(customFragmentAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        return true;
     }
 }
--- app/src/main/java/com/robop/attendancerecord/MainFragment.java
+++ app/src/main/java/com/robop/attendancerecord/MainFragment.java
@@ -1,15 +1,20 @@
 package com.robop.attendancerecord;
 
-import android.app.Fragment;
-
-/**
- * Created by daipc on 18/02/08.
- */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainFragment extends Fragment {
 
     public static MainFragment newInstance(){
         MainFragment fragment = new MainFragment();
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
